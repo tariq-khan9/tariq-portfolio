df75c9f2b6862edcb2f2dafc61ca00a94b71f2cd
tariq-khan9 <tariqkhan.cs9@gmail.com>
2023-10-05 21:00:41 +0500

    hero section done

--- src/App.jsx
+++ src/App.jsx
@@ -7,22 +7,24 @@ const  App = () => {
   return (
     
     <BrowserRouter>
-     <div className="relative z-0 bg-primary">
-        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
-          <Navbar/>
-          <Hero/>
+      <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+        <div className=''>
+          <Navbar />
+          <Hero />
         </div>
-        <About/>
-        <Experience/>
-        <Tech/>
-        <Works/>
-        <Feedbacks/>
-        <div className="relative z-0">
-          <Contact/>
-          <StarsCanvas/>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className='relative z-0'>
+          <Contact />
+          <StarsCanvas />
         </div>
       </div>
     </BrowserRouter>
+    
+
   )
 }
 
